ee8c0e641d59a6d619c8b032a19f60c489215f9e
DSpaans <d.c.spaans@student.tue.nl>
2025-01-06 22:26:52 +0100

    Update data.py

--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,10 +1,19 @@
 import plotly.express as px
+import pandas as pd
+#Note from Dembis: If ewe want to modify the data before it is used in the visualizations, 
+# it might be a good idea to do it beforehand and upload a 2nd file to the project if it takes a long time to process
 
 
-def get_data():
+def get_dummy_data():
     # Read data
     df = px.data.iris()
 
     # Any further data preprocessing can go her
 
+    return df
+
+def get_data():
+    # Read data
+    file = "..jbi100_app/ASI_Database.xlsx"
+    df = pd.read_excel(file)
     return df
